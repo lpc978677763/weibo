861669dd044317fe794a4f067867145b306bb8f9
lpc <978677763@qq.com>
2019-01-09 09:03:08 +0000

    基础页面

---
+++ resources/views/static_pages/help.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.default')
+@section('title','帮助')
+@section('content')
+<h1>帮助🦄</h1>
+@stop
---
+++ resources/views/static_pages/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.default')
+@section('title','主页')
+@section('content')
+<h1>主页🐎</h1>
+@stop
