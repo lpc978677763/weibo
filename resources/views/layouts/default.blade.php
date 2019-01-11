a5aa6473b6457edc3ad592922d3e9baa0458aea7
lpc <978677763@qq.com>
2019-01-11 09:39:38 +0000

    初始化样式

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,10 +1,23 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>@yield('title','Weibo App')-🐂🍺🦄</title>
-</head>
-<body>
-	@yield('content')
-</body>
+<html>
+  <head>
+    <title>@yield('title','Weibo App')-🐂🍺🦄</title>
+    <link rel="stylesheet" href="/css/app.css">
+  </head>
+  <body>
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div class="container">
+        <a class="navbar-brand" href="/">Weibo App</a>
+        <ul class="navbar-nav justify-content-end">
+          <li class="nav-item"><a class="nav-link" href="/help">帮助</a></li>
+          <li class="nav-item" ><a class="nav-link" href="#">登录</a></li>
+        </ul>
+      </div>
+    </nav>
+
+    <div class="container">
+      @yield('content')
+    </div>
+  </body>
 </html>
--- resources/views/static_pages/help.blade.php
+++ resources/views/static_pages/help.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.default')
 @section('title','帮助')
 @section('content')
-<h1>帮助🦄</h1>
+<h1>帮助</h1>
 @stop
