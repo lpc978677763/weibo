76afb9ef6d48e9362cbbc33894775f9b625ad764
lpc <978677763@qq.com>
2019-01-14 08:07:58 +0000

    session created

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,7 @@ Route::get('/about', 'StaticPagesController@about')->name('about');
 
 Route::get('signup', 'UsersController@create')->name('signup');
 Route::resource('users', 'UsersController');
-/*Route::get('/', function () {
-    return view('welcome');
-});*/
+
+Route::get('login', 'SessionsController@create')->name('login');
+Route::post('login', 'SessionsController@store')->name('login');
+Route::delete('logout', 'SessionsController@destroy')->name('logout');
